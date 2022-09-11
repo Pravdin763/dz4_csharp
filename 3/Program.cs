49a038beae88d72fff6ba20888d1ac74d757af73
igor <igor.pravdin163@gmail.com>
2022-09-11 22:58:52 +0400

    3

---
+++ 3/Program.cs
@@ -0,0 +1,13 @@
+﻿ // Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+
+ int i = 0;
+int[] array = new int[8];
+
+while (i < array.Length)
+{
+    Console.WriteLine($"Введите цело число: {i + 1} из 8");
+    array[i] = int.Parse(Console.ReadLine());
+    i += 1;
+}
+
+Console.WriteLine(string.Join(", ", array));
